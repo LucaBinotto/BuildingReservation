8cec00d8fc065f8a7f1fdc68dbf0d02f045a736a
LucaB <LucaB@DESKTOP-9ILK5NC.mshome.net>
2021-08-09 15:54:17 +0200

    initial commit

---
+++ EserciziNonaSettimana/src/main/java/it/epicode/be/EserciziNonaSettimanaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziNonaSettimanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserciziNonaSettimanaApplication.class, args);
+	}
+
+}
